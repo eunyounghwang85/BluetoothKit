5833a8e6c94e401b66efd0ba0a85aaae77d13f5d
eyhwang-imac <eyhwang@hdtel.co.kr>
2021-03-22 09:43:01 +0900

    dd

---
+++ Example/Source/View Controllers/Singleton.swift
@@ -0,0 +1,13 @@
+//
+//  Singleton.swift
+//  BKExample (iOS)
+//
+//  Created by eunyoung hwang on 2021/03/18.
+//  Copyright © 2021 Rasmus Taulborg Hummelmose. All rights reserved.
+//
+
+import UIKit
+
+class Singleton: NSObject {
+
+}
